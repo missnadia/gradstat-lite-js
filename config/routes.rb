caf9c5f25300b41c769352c80c338f76d9fc3986
learn <miss.nadia.lee@gmail.com>
2019-02-09 16:55:48 -0800

    Add routes to config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#home'
+  get '/signup' => 'students#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get 'student/:id/courses', to: 'student#courses'
+  get 'courses/total_lessons' => 'courses#total_lessons'
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+
+  resources :courses do
+    resources :comments
+  end
+  
+  resources :students
+  resources :charts, only: [:index]
+  resources :sessions, only: [:create, :destroy]
+  resource :static, only: [:home]
 end
