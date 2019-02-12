768767b087eb2a2ad3920e9fdd5f3b9e0b4cceb7
Nadia Lee <32117376+missnadia@users.noreply.github.com>
2019-02-12 14:28:37 -0500

    Create comments_controller.rb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,39 @@
+class CommentsController < ApplicationController
+    before_action :set_course, only: [:edit, :create, :update, :destroy]
+    before_action :set_comment, only: [:edit, :update, :destroy]
+
+    def new
+        @comment = Comment.new
+    end
+
+    def edit
+    end
+    
+    def create
+        @comment = @course.comments.create(params[:comment].permit(:comment))
+        redirect_to @course
+    end
+
+    def update
+        if @comment.update(comment: params[:comment])
+           redirect_to @course
+        else
+           render :edit
+        end
+    end
+  
+    def destroy
+        @comment.destroy
+        redirect_to @course
+    end
+
+    private
+
+    def set_course
+        @course = Course.find(params[:course_id])
+    end
+
+    def set_comment
+        @comment =  @course.comments.find(params[:id])
+    end
+end
