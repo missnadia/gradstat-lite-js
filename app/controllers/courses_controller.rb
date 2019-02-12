0be8223b2479a166e86681b5927ccc63b3074e41
Nadia Lee <32117376+missnadia@users.noreply.github.com>
2019-02-12 14:28:06 -0500

    Create courses_controller.rb

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,54 @@
+class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.save
+      redirect_to @course
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to @course
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to courses_path
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(
+      :name,
+      :time_spent,
+      :completed,
+      :date,
+      :student_id
+    )
+  end
+end
