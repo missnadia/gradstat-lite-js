d45ebd187447be275d1e6153627f300e3e64d94f
learn <miss.nadia.lee@gmail.com>
2019-02-12 18:20:46 -0800

    Add serializers to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :require_login, except: [:home, :new, :create]
     helper_method :current_user
+    include ActionController::Serialization
   
     private
   
