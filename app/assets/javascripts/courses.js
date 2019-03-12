2310ad1e3e30c050b21822d3f36414f8aa554e87
learn <miss.nadia.lee@gmail.com>
2019-03-11 17:56:30 -0700

    Add sort courses function

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -51,21 +51,21 @@ function sortCourses() {
       dataType: "json"
     }).done(function (data) {
       let courseData = $("div#ajax-course-data")
-      courseData.html("");
+      courseData.html("")
       courseData.append(Course.courseTableHeader())
       const sortedCourses = data.sort(function (a, b) {
-        var courseA = a.name.toUpperCase();
-        var courseB = b.name.toUpperCase();
+        var courseA = a.name.toUpperCase()
+        var courseB = b.name.toUpperCase()
         if (courseA < courseB) {
-          return -1;
+          return -1
         }
         if (courseA > courseB) {
-          return 1;
+          return 1
         }
-        return 0;
-      });
+        return 0
+      })
       sortedCourses.forEach(function (obj) {
-        let mycourse = new Course(obj);
+        let mycourse = new Course(obj)
         let myCourseHTML = mycourse.courseHTML()
         courseData.append(myCourseHTML)
       });
